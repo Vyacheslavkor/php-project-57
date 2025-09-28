1bb8199a1d83d08eae219b9fb5d5db4a4d3aef44
Vyacheslav Korolev <slavakorolev2@gmail.com>
2025-09-28 18:27:50 +0400

    Test commit.

--- resources/views/task_status/create.blade.php
+++ resources/views/task_status/create.blade.php
@@ -22,7 +22,7 @@
                             <div>
                                 <x-input-label for="name" :value="__('Имя')"/>
                                 <x-text-input id="name" name="name" type="text" class="mt-1 block w-full"
-                                              :value="old('name', $taskStatus->name)" required autofocus
+                                              :value="old('name', $taskStatus->name)" autofocus
                                               autocomplete="name"
                                 />
                                 <x-input-error class="mt-2" :messages="$errors->get('name')"/>
