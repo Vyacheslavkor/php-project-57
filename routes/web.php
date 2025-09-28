8261adfb9e5af412b5a48a498184deb095137c66
Vyacheslav Korolev <slavakorolev2@gmail.com>
2025-09-28 15:48:47 +0400

    Main page fixed.

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('dashboard');
-});
+    return view('dashboard');
+})->name('index');
 
 
 Route::get('/dashboard', function () {
