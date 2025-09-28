ccf4f402b4e317b2c91c1afa315dff0cefdfc905
Vyacheslav Korolev <slavakorolev2@gmail.com>
2025-09-28 17:50:27 +0400

    Password validating message changed.

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,10 +30,11 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'name'     => ['required', 'string', 'max:255'],
+            'email'    => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        ],
+            ['password.min' => 'Пароль должен иметь длину не менее 8 символов']);
 
         $user = User::create([
             'name' => $request->name,
