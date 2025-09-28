8b91f48c3c583b2b574ef274768e71f906ebc0bf
Vyacheslav Korolev <slavakorolev2@gmail.com>
2025-09-28 16:59:37 +0400

    Test spatie form.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+    {{ html()->modelForm((new App\Models\User())->toArray(), 'POST', route('register'))->open() }}
 
         <!-- Name -->
         <div>
@@ -44,7 +43,7 @@
                 {{ __('Уже зарегистрированы?') }}
             </a>
 
-            {{ html()->submit('Зарегистрировать')->value('Зарегистрировать')->class('ms-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150') }}
+            {{ html()->submit('Зарегистрировать')->class('ms-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150') }}
         </div>
-    </form>
+    {{ html()->closeModelForm() }}
 </x-guest-layout>
